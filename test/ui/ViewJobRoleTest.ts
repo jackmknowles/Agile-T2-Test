465ac918a35f5de53082c8020947cb66430d3cfe
jackknowles <jack.knowles@kainos.com>
2024-07-31 08:58:34 +0100

    Minor changes to ViewJobRolesTest

--- test/ui/ViewJobRoleTest.ts
+++ test/ui/ViewJobRoleTest.ts
@@ -1,51 +1,81 @@
-import { expect } from 'chai';
-import { JobRolesPage } from './ViewJobRolesPage';
-import { urlContains } from 'selenium-webdriver/lib/until';
-import { By } from 'selenium-webdriver';
+import { expect } from "chai";
+import { JobRolesPage } from "./ViewJobRolesPage";
+import { urlContains } from "selenium-webdriver/lib/until";
+import { By } from "selenium-webdriver";
 
-describe('Job Roles Page Tests', () => {
-  let jobRolesPage: JobRolesPage
+describe("Job Roles Page Tests", () => {
+  let jobRolesPage: JobRolesPage;
 
   //These are specifically for the tests, so I think these are better suited to remain in the test class, rather than going in the page class
-  const validLocations = ['BELFAST', 'DERRY', 'LONDON']; // This can be adatped as reuired
-  const validRoleNames = ['Software Engineer', 'Data Scientist', 'Cyber Security Analyst']; // This can be adapted as reuired
+  const validLocations = [
+    "BELFAST", 
+    "DERRY", 
+    "LONDON"
+  ]; // This can be adatped as reuired
+  const validRoleNames = [
+    "Software Engineer",
+    "Data Scientist",
+    "Cyber Security Analyst",
+  ]; // This can be adapted as reuired
 
   const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/; //this should remain as is, this is the date regex
 
-  //before any 
+  //before the test runs
+  //probably need to remove the driver from this and include it in a new base class
   before(async () => {
-    jobRolesPage = new JobRolesPage;
+    jobRolesPage = new JobRolesPage();
   });
 
+  // Closing the driver, this is calling the method from the jobRolesPage class
   after(async () => {
     await jobRolesPage.close();
   });
 
-  it.only('should display a table with the correct headings', async () => {
+  it.only("should display a table with the correct headings", async () => {
     await jobRolesPage.open();
     await jobRolesPage.waitForTable();
 
     const headerRow = await jobRolesPage.findHeaderRow();
-    const headers = await headerRow.findElements(By.css('th'));
-    const headerTexts = await Promise.all(headers.map(header => header.getText()));
-    const expectedHeaders = ['Job Role', 'Location', 'Band', 'Capability', 'Closing Date'];
+    const headers = await headerRow.findElements(By.css("th"));
+    const headerTexts = await Promise.all(
+      headers.map((header) => header.getText())
+    );
+    const expectedHeaders = [
+      "Job Role",
+      "Location",
+      "Band",
+      "Capability",
+      "Closing Date",
+    ];
 
-    expect(headerTexts).to.deep.equal(expectedHeaders, 'Table headers do not match expected headers');
+    expect(headerTexts).to.deep.equal(
+      expectedHeaders,
+      "Table headers do not match expected headers"
+    );
   });
-  
-  it('should display the correct job roles information', async () => {
+
+  it("should display the correct job roles information", async () => {
     await jobRolesPage.open();
     await jobRolesPage.waitForTable();
     const actualJobRoles = await jobRolesPage.getJobRoles();
 
     actualJobRoles.forEach((role, index) => {
-      expect(role.location).to.be.oneOf(validLocations, `Invalid location for role index ${index}`);
-      expect(role.roleName).to.be.oneOf(validRoleNames, `Invalid role name for role index ${index}`);
-      expect(role.closingDate).to.match(dateRegex, `Invalid date format for role index ${index}`);
+      expect(role.location).to.be.oneOf(
+        validLocations,
+        `Invalid location for role index ${index}`
+      );
+      expect(role.roleName).to.be.oneOf(
+        validRoleNames,
+        `Invalid role name for role index ${index}`
+      );
+      expect(role.closingDate).to.match(
+        dateRegex,
+        `Invalid date format for role index ${index}`
+      );
     });
   });
 
-  it('should have non-empty data fields for each job role', async () => {
+  it("should have non-empty data fields for each job role", async () => {
     await jobRolesPage.open();
     await jobRolesPage.waitForTable();
     const actualJobRoles = await jobRolesPage.getJobRoles();
@@ -59,34 +89,29 @@ describe('Job Roles Page Tests', () => {
     });
   });
 
-  it('should bring the user to the Instagram page', async () => {
+  it("should bring the user to the Instagram page", async () => {
     await jobRolesPage.open();
     await jobRolesPage.clickInstagramButton();
 
-    await jobRolesPage.driver.wait(urlContains('instagram'), 10000);
+    await jobRolesPage.driver.wait(urlContains("instagram"), 10000);
 
     const currentUrl = await jobRolesPage.driver.getCurrentUrl();
     console.log(currentUrl);
-    expect(currentUrl).to.include('instagram');
+    expect(currentUrl).to.include("instagram");
     await jobRolesPage.driver.navigate().back();
+  });
 
-});
+  it("should bring the user to the Facebook page", async () => {
+    await jobRolesPage.open();
+    await jobRolesPage.clickFacebookButton();
 
-it('should bring the user to the Facebook page', async () => {
-  await jobRolesPage.open();
-  await jobRolesPage.clickFacebookButton();
+    await jobRolesPage.driver.wait(urlContains("facebook"), 10000);
 
-  await jobRolesPage.driver.wait(urlContains('facebook'), 10000);
-
-  const currentUrl = await jobRolesPage.driver.getCurrentUrl();
-  console.log(currentUrl);
-  expect(currentUrl).to.include('instagram');
-  await jobRolesPage.driver.navigate().back();
-
-});
-
-
-
+    const currentUrl = await jobRolesPage.driver.getCurrentUrl();
+    console.log(currentUrl);
+    expect(currentUrl).to.include("instagram");
+    await jobRolesPage.driver.navigate().back();
+  });
 
   /*
   it('should display the correct job roles information', async () => {
